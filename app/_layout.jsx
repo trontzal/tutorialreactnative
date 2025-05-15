f65b8ab1f6549038e1cb47879dfab051b6421ba7
trontzal <trontzal@gmail.com>
2025-05-15 20:22:10 +0200

    temas light y dark funcional muy basico

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,12 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, useColorScheme, View } from 'react-native'
 import React from 'react'
 import { Slot, Stack } from 'expo-router'
+import { Colors } from '../constants/colors'
 
 const RootLayout = () => {
+  const  colorScheme = useColorScheme()
+  const theme = Colors[colorScheme] ?? Colors.light
+
   return (
       <Stack screenOptions={{
-        headerStyle:{ backgroundColor: '#ddd'},
-        headerTintColor:'#333'
+        headerStyle:{ backgroundColor: theme.backgroundColor},
+        headerTintColor: theme.title
       }}>
         <Stack.Screen name="index" options={{title: 'Home'}}/>
         <Stack.Screen name="about" options={{title: 'About'}}/>
---
+++ constants/colors.js
@@ -0,0 +1,18 @@
+import { ImageBackground } from "react-native";
+
+export const Colors = {
+    primary: "#6849a7",
+    warning: "#cc475a",
+
+    dark: {
+        text: "#d4d4d4",
+        title: "#fff",
+        background: "#2522231",
+    },
+
+    light: {
+        text: "#625f72",
+        title: "#201e2b",
+        background: "#e0dfe8"
+    }
+}
